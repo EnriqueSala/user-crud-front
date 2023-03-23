14cb3aa27eccdafe5a6e2aebaa7c133ac4b82046
kikegh <sala488@gmail.com>
2023-03-22 21:35:07 -0500

    updated api address

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://3.129.248.218:8080/crudUser/api",
+  baseURL: "http://3.129.248.218:8080/crudUser/api",
   headers: {
     "Content-type": "application/json"
   },
